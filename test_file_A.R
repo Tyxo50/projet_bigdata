a3b07a88d0b1e7d665cfd8f0b1b2e8ff5720d470
TaMLeNok <t.lelievre718@gmail.com>
2024-06-10 14:28:23 +0200

    clean csv

--- test_file_A.R
+++ test_file_A.R
@@ -1,6 +1,12 @@
 data = read.csv(file="Patrimoine_Arbore.csv", sep = ",")
 
 
+
+remove_string_of_column <- function(data, column, string) {
+  data[[column]] = gsub(string, "\"", data[[column]])
+  return(data)
+}
+
 to_utf8 <- function(data) {
     for (identifier in colnames(data)) {
         data[[identifier]] = iconv(data[[identifier]], from = "latin1", to = "UTF-8")
@@ -11,8 +17,7 @@ to_utf8 <- function(data) {
 convert_data <- function(data, types = c('numeric', 'numeric', 'integer', 'date', 'character', 'character', 'character', 'character', 'integer', 'numeric', 'numeric', 'integer', 'character', 'character', 'character', 'character', 'character', 'bool', 'character', 'date', 'integer', 'integer', 'integer', 'date', 'character', 'character', 'date', 'character', 'character', 'character', 'character', 'date', 'character', 'date', 'character', 'character', 'bool')) {
   i = 1
   for(col in colnames(data)){
-    print(i)
-    print(col)
+    
     if(types[i] == 'numeric'){
       data[[col]] = as.numeric(data[[col]])
     }else if (types[i] == 'integer'){
@@ -32,16 +37,13 @@ convert_data <- function(data, types = c('numeric', 'numeric', 'integer', 'date'
 }
 
 
+
 data = to_utf8(data)
 data2 = convert_data(data)
 
 # print(summary(data))
 # 
 sapply(data2, class)
-
-
-
-View(data)
 
 View(data2)
 
