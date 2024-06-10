6b2a9b8b8dea76c50428e032f6f54e9dbcf6b7b4
TaMLeNok <t.lelievre718@gmail.com>
2024-06-10 10:22:14 +0200

    utf8 numeric

--- test_file_A.R
+++ test_file_A.R
@@ -1,12 +1,23 @@
 data = read.csv(file="Patrimoine_Arbore.csv", sep = ",")
 
 
-data = iconv(data, from = "latin1", to = "UTF-8")
+to_utf8 <- function(data) {
+    for (identifier in colnames(data)) {
+        data[[identifier]] = iconv(data[[identifier]], from = "latin1", to = "UTF-8")
+    }
+    return(data)
+}
 
-print(head(data))
+convert_data <- function(data) {
+    for (identifier in colnames(data)) {
+        if (is.numeric(data[[identifier]])) {
+            data[[identifier]] = as.numeric(data[[identifier]])
+        }
+    }
+    return(data)
+}
+data = convert_to_integer(data)
 
-
-
-
+print(summary(data))
 
 
