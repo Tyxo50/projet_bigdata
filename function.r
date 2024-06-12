e16b05bbd1be967dc3fdf66fd09106941adbe814
TaMLeNok <t.lelievre718@gmail.com>
2024-06-12 10:34:49 +0200

    kkk

--- function.r
+++ function.r
@@ -152,5 +152,23 @@ get_quartier_from_coords <- function(data) {
 }
 
 
+predict_tronc_diam <- function(data) {
+  data_temp <- data[!is.na(data$haut_tronc) & !is.na(data$haut_tot) & !is.na(data$feuillage) & !is.na(data$fk_stadedev), ]
+  
+  model <- lm(tronc_diam ~ haut_tronc + haut_tot + fk_stadedev + feuillage, data = data_temp)
+  
+  rows_to_predict <- data[is.na(data$tronc_diam), ]
+  
+  if(nrow(rows_to_predict) > 0) {
+    predictions <- predict(model, newdata = rows_to_predict)
+    
+    data$tronc_diam[is.na(data$tronc_diam)] <- predictions
+  }
+  
+  return(data)
+}
+
+
+
 
 
