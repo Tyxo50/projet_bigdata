f66e5bb0e854552d4e628621c1104a6dfbdda2c4
eltar14 <lebouant2@gmail.com>
2024-06-12 13:44:45 +0200

    tolower dans le bon ordre

--- main.r
+++ main.r
@@ -4,8 +4,8 @@ data = read.csv(file="Patrimoine_Arbore.csv", sep = ",")
 print(paste("Data dim start",dim(data)))
 
 data = to_utf8(data)
-data = convert_data(data)
 data = to_lower(data)
+data = convert_data(data)
 data = put_na_if_empty(data)
 data = remove_na_on_x_y(data)
 data = remove_object_id(data)
@@ -14,6 +14,7 @@ data = remove_empty_line13(data)
 data = remove_na_feuillage(data, "feuillage")
 
 data = get_quartier_from_coords(data)
+data = to_factor(data)
 
 print(dim(data))
 print(dim(data))
